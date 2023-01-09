2559750489c81ddc99acc08e9f1c84aec505563c
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-01-09 23:21:58 +0900

    Test : Post Test
    - Related to: #12

---
+++ src/test/java/meong/nyang/post/PostTest.java
@@ -0,0 +1,201 @@
+package meong.nyang.post;
+
+import meong.nyang.domain.Member;
+import meong.nyang.domain.Post;
+import meong.nyang.repository.MemberRepository;
+import meong.nyang.repository.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+public class PostTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void 게시글작성() throws Exception {
+        //Given
+        Date nowDate = new Date();
+        SimpleDateFormat Today = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+        Member member = Member.builder()
+                .id(1L)
+                .password(null)
+                .nickname("만두냠")
+                .email("jung_j_yeon@naver.com")
+                .img(null)
+                .build();
+        Post post1 = Post.builder()
+                .id(1L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+        Post post2 = Post.builder()
+                .id(2L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!!!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해 라뷰라뷰류")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+
+        //When
+        Member member1 = memberRepository.save(member);
+        Post p1 = postRepository.save(post1);
+        Post p2 = postRepository.save(post2);
+
+        //Then
+        Post p = postRepository.findById(p1.getId()).get();
+        assertThat(p1.getTitle()).isEqualTo("울 귀여운 만둥이 보고가세요!");
+        System.out.println(p1.getContents());
+    }
+    @Test
+    void 게시글삭제() throws Exception {
+        //given
+        Date nowDate = new Date();
+        SimpleDateFormat Today = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+        Member member = Member.builder()
+                .id(1L)
+                .password(null)
+                .nickname("만두냠")
+                .email("jung_j_yeon@naver.com")
+                .img(null)
+                .build();
+        Post post1 = Post.builder()
+                .id(1L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+        Post post2 = Post.builder()
+                .id(2L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!!!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해 라뷰라뷰류")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+        Member m1 = memberRepository.save(member);
+        Post p1 = postRepository.save(post1);
+        Post p2 = postRepository.save(post2);
+        //when
+        postRepository.delete(postRepository.findById(1L).get());
+        //then
+    }
+
+    @Test
+    void 게시글조회() throws Exception {
+        //given
+        Date nowDate = new Date();
+        SimpleDateFormat Today = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+        Member member = Member.builder()
+                .id(1L)
+                .password(null)
+                .nickname("만두냠")
+                .email("jung_j_yeon@naver.com")
+                .img(null)
+                .build();
+        Post post1 = Post.builder()
+                .id(1L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+        Post post2 = Post.builder()
+                .id(2L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!!!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해 라뷰라뷰류")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+        //when
+        Member m1 = memberRepository.save(member);
+        Post p1 = postRepository.save(post1);
+        Post p2 = postRepository.save(post2);
+        //then
+        System.out.println(postRepository.findAll());
+        System.out.println(postRepository.findById(p1.getId()));
+    }
+
+    @Test
+    void 게시글업데이트() throws Exception {
+        //given
+        Date nowDate = new Date();
+        SimpleDateFormat Today = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+        Member member = Member.builder()
+                .id(1L)
+                .password(null)
+                .nickname("만두냠")
+                .email("jung_j_yeon@naver.com")
+                .img(null)
+                .build();
+        Post post1 = Post.builder()
+                .id(1L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+        Post post2 = Post.builder()
+                .id(2L)
+                .type(1L)
+                .category(1L)
+                .count(0L)
+                .title("울 귀여운 만둥이 보고가세요!!!")
+                .contents("웅냥냥 우리 귀여운 만두 너무너무 귀엽죠 우리집 내새끼 사랑해 라뷰라뷰류")
+                .date(Today.format(nowDate).toString())
+                .img(null)
+                .member(member)
+                .build();
+        //when
+        Member m1 = memberRepository.save(member);
+        Post p1 = postRepository.save(post1);
+        Post p2 = postRepository.save(post2);
+        //then
+        System.out.println(postRepository.findById(p1.getId()).toString());
+    }
+}
