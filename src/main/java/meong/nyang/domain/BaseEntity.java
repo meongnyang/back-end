a1a74c56070c928dfed851ed3ba697dde170f336
Jiyeon Jeong <jungjyeon@sju.ac.kr>
2023-01-12 13:32:50 +0900

    feat : Entity 추가
    - Base Entity : 날짜, 시간 분리
    
    - Related to : #4

---
+++ src/main/java/meong/nyang/domain/BaseEntity.java
@@ -0,0 +1,33 @@
+package meong.nyang.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @CreatedDate
+    private String createdDate;
+
+    @Column(name = "created_time", nullable = false, unique = false)
+    @CreatedDate
+    private String createdTime;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        this.createdTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+}
