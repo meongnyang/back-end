a3b8e6bf007f527c832a251793e71bb3e26e1fcf
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-01-17 07:13:28 +0900

    feat : 일반 회원가입 및 회원 닉네임 수정 기능
    일반 회원 가입 코드 및 회원 닉네임 수정 기능, 중복 검사 기능 완료
    
    Fixes : #10

--- src/main/java/meong/nyang/controller/MemberController.java
+++ src/main/java/meong/nyang/controller/MemberController.java
@@ -2,18 +2,49 @@ package meong.nyang.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import meong.nyang.domain.Member;
+import meong.nyang.dto.MemberRequestDto;
+import meong.nyang.dto.MemberResponseDto;
+import meong.nyang.repository.MemberRepository;
 import meong.nyang.service.MemberService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.Optional;
 
 @Log4j2
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+
+    @PostMapping("/members")
+    public ResponseEntity<MemberRequestDto> userSignUp(@RequestBody MemberRequestDto memberRequestDto) throws Exception {
+        Optional<Member> findMember = memberRepository.findMemberByEmail(memberRequestDto.getEmail());
+        try {
+            if (findMember.isPresent()) {
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+            memberService.createMember(memberRequestDto);
+            return new ResponseEntity<>(memberRequestDto, HttpStatus.CREATED);
+
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
 
+    @PatchMapping("/members/updateNickName/{memberId}")
+    public MemberResponseDto updateNickName(@PathVariable Long memberId,
+                                            @RequestBody MemberRequestDto memberRequestDto
+            , Member member) throws Exception {
+        Long findMemberId = memberService.updateInfo(memberRequestDto, memberId);
+        MemberResponseDto responseDto = memberService.findMemberByMemberId(findMemberId);
 
+        return responseDto;
+    }
 }
+
--- src/main/java/meong/nyang/repository/MemberRepository.java
+++ src/main/java/meong/nyang/repository/MemberRepository.java
@@ -4,11 +4,13 @@ import meong.nyang.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
-    Member findMemberByEmail(String email);
+    Optional<Member> findMemberByEmail(String email);
+    Member findMemberById(Long id);
+    Optional<Member> findMemberByNickname(String nickname);
 }
