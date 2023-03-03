dce1bb598e6f8fffc840180d278561d2e6d61d67
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-03-03 11:39:06 +0900

    chore : Test 주석처리

--- src/test/java/meong/nyang/NyangApplicationTests.java
+++ src/test/java/meong/nyang/NyangApplicationTests.java
@@ -1,13 +1,13 @@
-package meong.nyang;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NyangApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package meong.nyang;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NyangApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
