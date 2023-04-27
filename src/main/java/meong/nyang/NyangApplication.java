0881dc33c62bdfa5fcdf1993819f681bc000a78b
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-04-27 19:44:53 +0900

    feat : 서버 시간 변경

--- src/main/java/meong/nyang/NyangApplication.java
+++ src/main/java/meong/nyang/NyangApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class NyangApplication {
-
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(NyangApplication.class, args);
 	}
